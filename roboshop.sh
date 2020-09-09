a423730ea08628e5fb6892ff3057321252cded3e
mmislamqa <mmislamqa@outlook.com>
2020-09-09 17:57:06 -0400

    commiting print hello

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ case $USER_ID in
   echo "Start Installation"
   ;;
 *)
-  echo -e "\e[31mYou should be a root user to perform this script\e[0m"
+  echo -e "\e[1;31mYou should be a root user to perform this script\e[0m"
   exit
 esac
 
