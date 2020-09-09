81ab3287bfcd13bcf5aecdeb8b7c288cfebd51f4
mmislamqa <mmislamqa@outlook.com>
2020-09-09 17:32:45 -0400

    commiting print hello

---
+++ roboshop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+case $1 in
+frontend)
+  echo Installing Frontend
+  echo Completed Installing Frontend
+  ;;
+catalogue)
+  echo Installing catalogue
+  echo Completed Installing catalogue
+  ;;
+cart)
+  echo Installing cart
+  echo Completed Installing cart
+  ;;
+esac
