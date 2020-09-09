4a0a7b27cab54dce831d1b8f45da25cac521d693
mmislamqa <mmislamqa@outlook.com>
2020-09-09 12:47:53 -0400

    commiting print hello

--- 02-print.sh
+++ 02-print.sh
@@ -4,3 +4,4 @@ echo Hello world
 echo -e "Hi, I am Moynul Islam\nI am learning devops."
 #For bold
 echo -e "\e[1mHello world to print bold text\e[0m"
+echo -e "\e[4mHello world to print underline text\e[0m"
