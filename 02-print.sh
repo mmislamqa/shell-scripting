f526ff803fdcc1f3771db0c4967e17e013e847be
mmislamqa <mmislamqa@outlook.com>
2020-09-09 12:52:37 -0400

    commiting print hello

--- 02-print.sh
+++ 02-print.sh
@@ -8,3 +8,4 @@ echo -e "\e[4mHello world to print underline text\e[0m"
 
 echo -e "\e[31mHello world to print red text\e[0m"
 echo -e "\e[32mHello world to print green text\e[0m"
+echo -e "Hello world to print yellow color only \e[33myellow text\e[0m"
