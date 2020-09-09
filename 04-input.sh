acd82ce65a61b654e2bfcea6e60badfa9d666ea8
mmislamqa <mmislamqa@outlook.com>
2020-09-09 14:19:52 -0400

    commiting print hello

--- 04-input.sh
+++ 04-input.sh
@@ -3,3 +3,12 @@
 #1 While executing
 read -p 'Enter your name: ' name
 echo "Hello $name, Welcome to USA"
+
+#2 Before Executing
+
+echo Script name = $0
+echo First Argument = $1
+echo second Argument = $2
+echo All arguments = $*
+echo  All Arguments = $@
+echo Number of Arguments = $#
