2728d69d88199bd2e3f34c39487c9b2134954ee9
mmislamqa <mmislamqa@outlook.com>
2020-09-10 17:27:58 -0400

    commiting print hello

--- roboshop.sh
+++ roboshop.sh
@@ -72,9 +72,9 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
   Print "Update MongoDB Configuration"
   sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
   Status_Check
-
-#  systemctl enable mongod
-#  systemctl start mongod
+  Print "Starting MongoDB Service"
+  systemctl enable mongod
+  systemctl start mongod
   ;;
 *)
   echo "Invalid Input, Please enter following input"
