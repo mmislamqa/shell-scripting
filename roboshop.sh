49aec8341c77a5141e2b76959e4c6df3e7fbd461
mmislamqa <mmislamqa@outlook.com>
2020-09-10 17:36:02 -0400

    commiting print hello

--- roboshop.sh
+++ roboshop.sh
@@ -75,6 +75,21 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
   Print "Starting MongoDB Service"
   systemctl enable mongod
   systemctl start mongod
+  Status_Check
+
+  Print "Downloading Schema"
+  curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+  Status_Check
+  cd /tmp
+  Print "Extracting Achives"
+  unzip mongodb.zip
+  Print "Load catalogue schema"
+  mongo < catalogue.js
+  Print "Load Users Schema"
+  mongo < users.js
+  Status_Check
+
+
   ;;
 *)
   echo "Invalid Input, Please enter following input"
